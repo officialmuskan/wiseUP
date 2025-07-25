3ebc068da5d4974c01a0e1a1b1056b724c9da547
Muskan <muskanchhabra.0806@gmail.com>
2025-07-25 21:05:12 +0530

    fial

--- server/server.js
+++ server/server.js
@@ -16,11 +16,7 @@ const port = process.env.PORT || 5000;
 
 
 // Middlewares
-app.use(cors({
-  origin: "http://localhost:5173", // or "*"
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-}));
+app.use(cors());
 app.use(clerkMiddleware())
 
 // Connect to MongoDB
