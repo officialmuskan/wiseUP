ff193730f30e6d187e80eb62832dbbfcc051a4f3
Muskan <muskanchhabra.0806@gmail.com>
2025-07-26 17:27:43 +0530

    key

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = "pk_test_Z3VpZGluZy1nYXItNzAuY2xlcmsuYWNjb3VudHMuZGV2JA"
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
