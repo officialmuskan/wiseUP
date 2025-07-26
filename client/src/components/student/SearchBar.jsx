a0580e5af1255c6d15da18b5b80bd66e476c56a3
Muskan <muskanchhabra.0806@gmail.com>
2025-07-26 21:28:35 +0530

    b

--- client/src/components/student/SearchBar.jsx
+++ client/src/components/student/SearchBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
 import { useNavigate } from 'react-router-dom';
 
-const SearchBar = ({data}) => {
+const SearchBar = ({data})=> {
 
   const navigate = useNavigate();
   const [input, setInput] = useState(data ? data : '');
